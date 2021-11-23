2628316d6aa9df4293bae7114d3546cf2f8c08f7
jaowie <94178771+jaowie@users.noreply.github.com>
2021-11-23 12:54:25 +0800

    daghan kog giusab

---
+++ customerHome.php
@@ -0,0 +1,12 @@
+<?php include 'server.php';
+include('SampleCrud/dbconnector.php');?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1><?php echo $_SESSION['FirstName']; ?></h1>
+  </body>
+</html>
