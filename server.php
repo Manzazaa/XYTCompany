d8f1fcf3b3718c32ff61f4fa0c5d4cc5527fcbf3
MentalBreakdance <94178856+lhansvirgil0123@users.noreply.github.com>
2021-11-23 13:01:48 +0800

    Revert "Revert "daghan kog giusab""
    This reverts commit 6e76aef474452ea89fafa72ffb7455a7285da769.

--- server.php
+++ server.php
@@ -2,19 +2,19 @@
 include('SampleCrud/dbconnector.php');
 session_start();
 
-//this is for admin login
+//this is for customerLogin
 	if (isset($_POST['loginbtn'])){
 		$username = $_POST['uname'];
 		$Password = $_POST['pass'];
 		$err = array('error'=>'');
-		$sqlcheck = "SELECT Name, Pass FROM admin";
+		$sqlcheck = "SELECT FirstName, Email, pass FROM customer";
 		$r_sqlcheck = mysqli_query($dbcon, $sqlcheck);
-		$Admins = mysqli_fetch_all($r_sqlcheck, MYSQLI_ASSOC);
+		$Customers = mysqli_fetch_all($r_sqlcheck, MYSQLI_ASSOC);
 
-		foreach ($Admins as $Admin) {
-	    if($_POST['uname'] == $Admin['Name'] and $_POST['pass'] == $Admin['Pass']){
-	      $_SESSION['AdminName'] = $Admin['Name'];
-	      header("location:home.php");
+		foreach ($Customers as $Customer) {
+	    if($_POST['uname'] == $Customer['Email'] and $_POST['pass'] == $Customer['pass']){
+	      $_SESSION['FirstName'] = $Customer['FirstName'];
+	      header("location:customerHome.php");
 	    }
 	  }
 	}
