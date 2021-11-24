b555fb3d2f223d3b8c9bc99fbdfcc25b0f18dfd2
MentalBreakdance <94178856+lhansvirgil0123@users.noreply.github.com>
2021-11-24 15:00:12 +0800

    Revert "Revert "admin/stock system""
    This reverts commit 94c98e8be3b7ac9292a5b7b68fae54c584e24a05.

--- removeFromCart.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$value_to_delete =$_GET['id'];
-if (($key = array_search($value_to_delete, $_SESSION['cart'])) !== false)//looks for the specific value to delete and unsets it
-    unset($_SESSION['cart'][$key]);
-	header('location: cart.php');
-?>
