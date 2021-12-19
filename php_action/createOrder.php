378b006bad729b381fd58cad20c4999ebc23eb2d
MentalBreakdance <94178856+lhansvirgil0123@users.noreply.github.com>
2021-12-19 21:23:10 +0800

    ....

--- php_action/createOrder.php
+++ php_action/createOrder.php
@@ -17,8 +17,7 @@ if($_POST) {
   $paid 								= $_POST['paid'];
   $dueValue 						= $_POST['dueValue'];
   $paymentType 					= $_POST['paymentType'];
-  $paymentStatus 				= $_POST['paymentStatus'];
-  $productName					= $_POST['productName[]']; 	
+  $paymentStatus 				= $_POST['paymentStatus'];	
 
 				
 	$sql = "INSERT INTO orders (order_date, client_name, client_contact, sub_total, vat, total_amount, discount, grand_total, paid, due, payment_type, payment_status, order_status) VALUES ('$orderDate', '$clientName', '$clientContact', '$subTotalValue', '$vatValue', '$totalAmountValue', '$discount', '$grandTotalValue', '$paid', '$dueValue', $paymentType, $paymentStatus, 1)";
