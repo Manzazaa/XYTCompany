aecc90687a00ca2225aecced18de475dd8436432
MentalBreakdance <94178856+lhansvirgil0123@users.noreply.github.com>
2021-12-26 15:27:20 +0800

    revert all

--- supplyorder.php
+++ supplyorder.php
@@ -127,11 +127,8 @@ if($_GET['o'] == 'add') {
 			    <div class="col-sm-offset-2 col-sm-10">
 			    <button type="button" class="btn btn-default" onclick="addRow()" id="addRowBtn" data-loading-text="Loading..."> <i class="glyphicon glyphicon-plus-sign"></i> Add Row </button>
 
-<<<<<<< HEAD
 			      <button type="submit" id="createOrderBtn" name="createOrderBtn" data-loading-text="Loading..." class="btn btn-success"><i class="glyphicon glyphicon-ok-sign"></i> Add Supplies</button>
-=======
 			      <button type="submit" id="createOrderBtn" data-loading-text="Loading..." class="btn btn-success"><i class="glyphicon glyphicon-ok-sign"></i> Save Changes</button>
->>>>>>> parent of 8f11731 (add supplies (UI))
 
 			      <button type="reset" class="btn btn-default" onclick="resetOrderForm()"><i class="glyphicon glyphicon-erase"></i> Reset</button>
 			    </div>
